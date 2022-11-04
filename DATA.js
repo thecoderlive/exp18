f48ce0e677f25e8822a88a54f3f6411d76630dc4
The Coder <82015917+thecoderlive@users.noreply.github.com>
2022-11-05 02:41:29 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,27 @@
+export const item = {
+    "id": "e6a81985-7657-4089-a92b-57e46bd61d90",
+    "gallery": [
+        {
+            "id": "c485a1f3-de1a-4e61-89f9-bb5573e2833a",
+            "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/36.png"
+        },
+        {
+            "id": "cd774776-f703-4fdb-9eda-7842948e395c",
+            "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/360.png"
+        },
+        {
+            "id": "10ddd0ec-6b51-422b-ac42-fc6c423e3a41",
+            "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/15.png"
+        },
+        {
+            "id": "b69885d0-0023-42a5-930b-861f749d563c",
+            "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/330.png"
+        },
+        {
+            "id": "ae696480-5c50-44c5-b43f-e6ce74559d3c",
+            "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/282.png"
+        }
+    ],
+    "event_name": "everyone",
+    "event_description": "Maybe may billion avoid attention once someone message. Position issue more although clear add. Health medical cover staff reduce kind."
+}
